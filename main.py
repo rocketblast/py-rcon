5f183a010579a33d8c93cddc8128224060c39d29
asabla <sparven89@gmail.com>
2014-06-08 05:02:47 +0200

    Added startup arguments
    - Added startup arguments
    - Also added assignment checks for arguments
    - Rearranged structure for starting up logghandler
    - Added some error handling
    - Fixed some tiny things
    - Added a goat to the collection
    - Bought a bottle of wine...could be jesus blood....

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from games.battlefield import BF4Handler
 
 logg = None
 
-def main(logs=""):
+def main(configfile="", logslocation="", serverfolder="", serverfile="", debug=False):
     # Logs
     ########################################################################
     runningpath = os.path.dirname(__file__)
@@ -25,37 +25,54 @@ def main(logs=""):
     threads = []    #array were we store all threads
 
     # If log directory is not set, then just create logs were py-rcon is running
-    if logs == "":
-        logdir = '{}\\logs\\py-rcon.log'.format(runningpath)
-        logg = LoggHandler.setup_logger('py-rcon', r'{}'.format(logdir))
-    else:
-        logdir = '{}\\py-rcon.log'.format(logs)
-        logg = LoggHandler.setup_logger('py-rcon', r'{}'.format(logdir))
     ########################################################################
+    if debug == True:
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.INFO
+
+    if logslocation == "":
+        logdir = '{}\\logs\\py-rcon.log'.format(runningpath)
+        logg = LoggHandler.setup_logger('py-rcon', r'{}'.format(logdir), loglevel)
+    else:
+        logdir = '{}\\py-rcon.log'.format(logslocation)
+        logg = LoggHandler.setup_logger('py-rcon', r'{}'.format(logdir), loglevel)
+    ########################################################################
+
+    logg.info("Starting up py-rcon...")
+    logg.debug("Debug has been activated!")
+    logg.info("---------------------------")
 
     # Read server config/s
     ########################################################################
-    settings = ConfigHandler.getSection('{}\config.ini'.format(runningpath), 'py-rcon')
+    if configfile == "" and serverfolder == "" or serverfile == "":  #Nothing is specified, GO DEFAULT!
+        #ADD CHECKS FOR: create file if not exsists, if exsist try to parse it!
+        logg.debug("No arguments given, goes for default settings")
+        settings = ConfigHandler.getSection('{}\config.ini'.format(runningpath), 'py-rcon', logg)
+    elif configfile != "":  #configfile is given, tries to load it
+        settings = ConfigHandler.getSection('{}', logg)
+    else:
+        logg.warn("Unable to figure out what to do, report this problem")
 
     # Single server file, with possible multiple server configs
     ###########################################
     #if serverfile isset, then tries to load it
-    if settings["serverfile"] != "":
+    if settings["serverfile"] != None:
         try:
             sections = ConfigHandler.getAllSections(settings["serverfile"], logg)
-            logg.info("Loaded serverfile")
+            logg.info("Loaded serverfile: {}".format(settings["serverfile"]))
 
             if sections:
                 # Tries to load all server configs inside config-file
                 for sect in sections:
-                    if sect.startswith("Server"):   # To make sure it only loads server configs
+                    if sect.lower().startswith("server"):   # To make sure it only loads server configs
                         s = ConfigHandler.getSection(settings["serverfile"], sect, logg)
 
                         # Checks for supported gameserver types
                         if s["game"] == "battlefield4":
                             t = BF4Handler('battlefield4', s["name"], s["ip"], int(s["port"]), s["password"], plugins=s["plugins"].split(' '))
                             threads.append(t)
-                            logg.info('[{}:{}] <{}> - Added server to list'.format(s["ip"], s["port"], s["name"]))
+                            logg.debug('[{}:{}] <{}> - Added server to list'.format(s["ip"], s["port"], s["name"]))
                         elif s["game"] == "battlefield3":
                             print "Found Battlefield 3 game"
                         else:
@@ -64,6 +81,7 @@ def main(logs=""):
                 logg.info("Unable to find anything in serverfile")
         except Exception as ex:
             logg.info("Unable to load serverfile: {}".format(settings["serverfile"]))
+            logg.error("Error: {}".format(ex))
     else:
         logg.info("There is no serverfile specified")
     ###########################################
@@ -92,14 +110,13 @@ def main(logs=""):
         logg.info("There is no serverfolder specified")
 
     ###########################################
+
+    # Start each thread
     ########################################################################
-
-
     for t in threads:
     	t.daemon = True
     	t.start()
     ########################################################################
-    
 
     # Run program, and wait for crash or user interupt (Ctrl + C)
     ########################################################################
@@ -112,14 +129,30 @@ def main(logs=""):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Py-rcon v1.0 by Rocketblast")
+    parser = argparse.ArgumentParser(description='''Py-rcon v0.9 by Rocketblast 2014, a mini-framework for handling connections 
+        to different types of gameservers.''') #come up with a better description...
     parser.add_argument('-c', '--config', metavar='file', type=argparse.FileType('r'), 
-        help='Configuration file')
+        help='''configuration file for py-rcon, if non given then log, server or server directory must be specified. 
+
+        Example: main.py -l C:\logs -sf C:\configs''')
+    parser.add_argument('-l', '--log', metavar='folder', help='Location were your log files will be stored')
+    parser.add_argument('-sf', '--serverfile', metavar='file', type=argparse.FileType('r'),
+        help='Location of the single server configuration file (multiple server configs in one file')
+    parser.add_argument('-sd', '--serverdirectory', metavar='folder', 
+        help='Path to a directory containing server configuration files')
+    parser.add_argument('-d', '--debug', 
+        help='If set to true, console will print out more status messages. Can be used for debbuging your plugin')
 
     args = parser.parse_args()
 
-    if args.config != None:
-        config = json.load(args.config)
-        main("", config)
+    if args.config != "" or args.log != "" or serverfile != "" or serverdirectory != "" or debug != None:
+        #config = json.load(args.config)
+        config = json.load(args.config) if args.config != None else ""
+        log = json.load(args.log) if args.log != None else ""
+        serverfile = json.load(args.serverfile) if args.serverfile != None else ""
+        serverdirectory = json.load(args.serverdirectory) if args.serverdirectory != None else ""
+        debug = bool(args.debug) if bool(args.debug) == True else False
+        
+        main(config, log, serverfile, serverdirectory, debug)
     else:
         main()
